c652fd07a429dd72f1a13dbaa55b63b45e24fd9d
Mark van der Loo <mark.vanderloo@gmail.com>
2017-10-06 15:49:13 +0000

    version 0.1.1

---
+++ R/modifier.R
@@ -0,0 +1,114 @@
+
+
+#' Store modification rules
+#'
+#' @keywords internal
+setRefClass("modifier"
+    , contains = "expressionset"
+    , methods = list(
+      show = function() show_modifier(.self)
+      , initialize = function(..., .file) ini_modifier(.self, ..., .file=.file)
+    ) 
+)
+
+
+show_modifier <- function(obj){
+  cat(sprintf("Object of class %s with %d elements:\n",class(obj)[1],length(obj)))
+  nm <- names(obj)
+  lb <- label(obj)
+  calls <- lapply(obj$rules,expr)
+  
+  for ( i in seq_along(calls)){
+    cat(sprintf("%s: %s\n  %s\n\n",nm[i],lb[i],gsub("\n","\n  ",as.character(calls[i]))  ))
+  }
+  
+}
+
+call2text <- function(cl){
+  capture.output(print(cl))
+}
+
+
+#' Create or read a set of data modification rules
+#'
+#'
+#' @param ... A comma-separated list of modification rules.
+#' @param .file A character vector of file locations.
+#'
+#' @return An object of class \code{modifier}.
+#'
+#' @examples 
+#' m <- modifier( if (height < mean(height)) height <- 2*height
+#' , if ( weight > mean(weight) ) weight <- weight/2  )
+#' modify(women,m)
+#' 
+#' 
+#' @export
+modifier <- function(..., .file) new("modifier", ... , .file=.file)
+
+
+
+
+ini_modifier <- function(obj ,..., .file){
+  if ( missing(.file) ){
+    validate::.ini_expressionset_cli(obj, ..., .prefix="M")
+  } else {
+    validate::.ini_expressionset_yml(obj, file=.file, .prefix="M")
+  }
+  
+  i <- sapply(expr(obj),is_modifying)
+  if ( !all(i) ){
+    err <- paste(sprintf("\n[%03d] %s", which(!i), sapply(expr(obj)[!i], call2text )))
+    warning(paste0(
+      "Invalid syntax detected. The following expressions have been ignored:",
+      paste(err,collapse="") 
+      ))
+  }
+  obj$rules <- obj$rules[i]
+  obj$._options <- validate::.PKGOPT
+  obj$._options(lin.eq.eps=0, lin.ineq.eps=0)
+  obj
+}
+
+# cl: a call.
+is_modifying <- function(cl){
+  # assignment or transient assignment (macro)
+  if (deparse(cl[[1]]) %in% c("<-","=", ":=")) return(TRUE)
+  
+  # if statement. if-else currently not supported
+  if ( cl[[1]] == "if" & length(cl) < 4 ) 
+    return(all(sapply(cl[-c(1,2)],is_modifying)))
+  
+  # block
+  if (cl[[1]]=="{") return(all(sapply(cl[-1],is_modifying)))
+  
+  
+  FALSE
+}
+
+#### S4 GENERICS --------------------------------------------------------------
+
+
+#' Modify a data set
+#'
+#' @param dat An R object carrying data
+#' @param x An R object carrying modififying rules
+#' @param ... Options.
+#'
+#' @export
+setGeneric("modify", function(dat, x, ...) standardGeneric("modify"))
+
+
+setGeneric("expr", function(x,...) standardGeneric("expr"))
+
+#### S4 IMPLEMENTATIONS
+setMethod("expr","modifier",function(x,...){
+  lapply(x$rules, function(r) r@expr)
+})
+
+
+
+
+
+
+
---
+++ R/modify.R
@@ -0,0 +1,93 @@
+#' Data Modification By Modifying Rules
+#' 
+#' @section Introduction:
+#' Data often contain errors and missing data. Experts can often correct
+#' commonly occuring errors based on simple conditional rules. This package
+#' facilitates the expression, management, and application of such rules on data
+#' sets.
+#' 
+#' 
+#' The general workflow in \code{dcmodify} follows the following pattern.
+#' \itemize{
+#'   \item Define or read a set of rules with \code{\link{modifier}}. 
+#'   \item \code{\link{modify}} data with the modification rules.
+#'   \item Examine the results either graphically or by summary.
+#' }
+#'
+#' There are several convenience functions that allow one to define modification
+#' rules from the commandline, through a (freeform or yaml) file and to
+#' investigate and maintain the rules themselves. Please have a look at the
+#' introductory vignette
+#'
+#' \code{vignette("introduction",package="dcmodify")}
+#'
+#'
+#'    
+#' @docType package
+#' @name dcmodify
+#' @aliases dcmodify-package dcmodify
+#' @import methods
+#' @import yaml
+#' @import validate
+#' @importFrom settings clone_and_merge
+#' @importFrom utils capture.output
+#' @export origin 
+#' @export description
+#' @export label
+#' @export variables
+NULL
+
+
+#' @include modifier.R
+NULL
+
+
+get_rule_guard <- function(r,dat, na.condition){
+  g <- guard(r)
+  I <- eval(g,dat)
+  if ( is.null(I) ){ 
+    rep(TRUE,nrow(dat)) 
+  } else {
+    I[is.na(I)] <- na.condition
+    I
+  }
+}
+
+#' @rdname modify
+#' @examples 
+#' m <- modifier( if (height < mean(height)) height <- 2*height
+#' , if ( weight > mean(weight) ) weight <- weight/2  )
+#' modify(women,m)
+#' @export 
+setMethod("modify",c("data.frame","modifier"), function(dat, x, ...){
+  opts <- settings::clone_and_merge(x$._options,...)
+  sequential <- opts("sequential")
+  odat <- if (sequential) NULL else dat
+  na.condition <- opts("na.condition")
+  
+  modifiers <- x$exprs(vectorize=FALSE
+    , expand_assignments=TRUE)
+  for ( m in modifiers ){
+    m <- set_guards(m)
+    for (n in m){ # loop over nested conditionals
+      I <- if (sequential) get_rule_guard(n, dat,na.condition) else get_rule_guard(n,odat,na.condition)
+      if (any(I)) dat[I,] <- within(dat[I,,drop=FALSE], eval(n))
+    }
+  }
+  
+  dat
+})
+
+#' Shortcut to modify data
+#' 
+#' @param dat A \code{data.frame}
+#' @param ... A comma-separated list of modifying rules.
+#' 
+#' @export 
+modify_so <- function(dat, ...){
+  modify(dat, modifier(...))
+}
+
+
+
+
